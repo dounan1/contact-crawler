ae07541aed0a3260b610b9f1338763364d171b0f
Dounan <dounan1@gmail.com>
2015-08-10 06:16:35 +0800

    abstracted results writer to a class variable

--- result.rb
+++ result.rb
@@ -1,14 +1,16 @@
 class Result
+  @@WRITER = CsvWriter
+
   class << self
 
     def store(site, username, page, emails, form)
       page_url = page.url.to_s
 
       emails.each do |email|
-        CsvWriter.write([[ username, site , email, page_url, nil ]])
+        @@WRITER.write([[ username, site , email, page_url, nil ]])
       end
 
-      CsvWriter.write([[ username, site , nil, page_url, form ]]) unless form.nil?
+      @@WRITER.write([[ username, site , nil, page_url, form ]]) unless form.nil?
     end
 
   end
