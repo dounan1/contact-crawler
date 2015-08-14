806b58efdbd3f87aea6c67123ed11501863e230f
Dounan <dounan1@gmail.com>
2015-08-15 07:19:34 +0800

    added accessor to Result@@writer

--- result.rb
+++ result.rb
@@ -1,5 +1,5 @@
 class Result
-  @@WRITER = CsvWriter
+  @@writer = CsvWriter
 
   class << self
 
@@ -7,10 +7,14 @@ class Result
       page_url = page.url.to_s
 
       emails.each do |email|
-        @@WRITER.write([[ username, site , email, page_url, nil ]])
+        @@writer.write([[ username, site , email, page_url, nil ]])
       end
 
-      @@WRITER.write([[ username, site , nil, page_url, form ]]) unless form.nil?
+      @@writer.write([[ username, site , nil, page_url, form ]]) unless form.nil?
+    end
+
+    def writer
+      @@writer
     end
 
   end
