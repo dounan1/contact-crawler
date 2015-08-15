feeeca4009a3e49d0a9e2a935e59bb9f8264d92e
Dounan <dounan1@gmail.com>
2015-08-15 18:35:25 +0800

    changed Results#writer class variable to class instance variable to allow it to be set by another class

--- result.rb
+++ result.rb
@@ -1,21 +1,17 @@
 class Result
-  @@writer = CsvWriter
+  @writer = CsvWriter
 
   class << self
+    attr_accessor :writer
 
     def store(site, username, page, emails, form)
       page_url = page.url.to_s
 
       emails.each do |email|
-        @@writer.write([[ username, site , email, page_url, nil ]])
+        writer.write([[ username, site , email, page_url, nil ]])
       end
 
-      @@writer.write([[ username, site , nil, page_url, form ]]) unless form.nil?
+      writer.write([[ username, site , nil, page_url, form ]]) unless form.nil?
     end
-
-    def writer
-      @@writer
-    end
-
   end
 end
