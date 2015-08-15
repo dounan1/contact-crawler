7ba499d7083e1131cc9643ee6c6cbe6281e638c9
Dounan <dounan1@gmail.com>
2015-08-16 01:22:26 +0800

    rewrote Results as a singleton instance to allow setting of writer without using class variables

--- result.rb
+++ result.rb
@@ -1,17 +1,21 @@
 class Result
-  @writer = CsvWriter
+  attr_accessor :writer
 
-  class << self
-    attr_accessor :writer
+  def self.result
+    @result ||= Result.new
+  end
 
-    def store(site, username, page, emails, form)
-      page_url = page.url.to_s
+  def self.store(site, username, page, emails, form)
+    page_url = page.url.to_s
 
-      emails.each do |email|
-        writer.write([[ username, site , email, page_url, nil ]])
-      end
-
-      writer.write([[ username, site , nil, page_url, form ]]) unless form.nil?
+    emails.each do |email|
+      result.writer.write([[ username, site , email, page_url, nil ]])
     end
+
+    result.writer.write([[ username, site , nil, page_url, form ]]) unless form.nil?
+  end
+
+  def initialize
+    @writer = CsvWriter
   end
 end
